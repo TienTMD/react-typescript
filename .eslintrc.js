292db6dce382d5c2bfc9561c391576200641271f
TienTMD <tien.hopthudientu@gmail.com>
2023-03-14 10:45:38 +0700

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
@@ -10,5 +11,6 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
+  ignorePatterns: ['vendor/', 'build/', 'public/build', 'coverage/'],
   root: true,
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+
 export default function Home(props) {
+  const [count, setCount] = useState<number>(0);
+
+  const getName = (name: string) => {
+    return `Username: ${name}`;
+  };
+
+  const onClick = () => {
+    setCount((prev: number) => prev + 1);
+  }
+
+  useEffect(() => {
+    getName();
+  }, []);
+
   return <div>Home</div>;
 }
